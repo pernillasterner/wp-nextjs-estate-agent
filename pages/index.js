524a9441c1bb583e00e530e3780c0816b5aa9119
Pernilla Sterner <psterner@hotmail.com>
2024-05-14 15:58:54 +0200

    Add query blocks accociated with homepage

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,29 @@
-export default function Home() {
+import { gql } from "@apollo/client";
+import client from "client";
+
+export default function Home(props) {
+  console.log(props);
   return <div>Next JS &amp; WordPress course.</div>;
 }
+
+export const getStaticProps = async () => {
+  const { data } = await client.query({
+    // Query blocks associated with homepage
+    query: gql`
+      query NewQuery {
+        nodeByUri(uri: "/") {
+          ... on Page {
+            id
+            blocks(postTemplate: false)
+          }
+        }
+      }
+    `,
+  });
+  return {
+    props: {
+      blocks: data.nodeByUri.blocks,
+      myexampleprop: "test",
+    },
+  };
+};
